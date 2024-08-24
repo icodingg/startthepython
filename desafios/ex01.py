e0a9b6c4a9a13530a65865dfaeac84f667578d25
kaue rodrigues <iggcode@gmail.com>
2024-08-24 20:51:33 -0300

    exercicios

---
+++ desafios/ex01.py
@@ -0,0 +1,27 @@
+#exercício 1
+n = int(input('Digite um número:'))
+n1 = n + 1
+n2 = n - 1
+print('O numero digitado é', n, ', seu sucessor é', n1, 'e seu antecessor:', n2)
+
+#exercício 2
+n = float(input('Digite um número:'))
+n1 = n * 2
+n2 = n * 3
+n3 = n ** (1/2)
+print('o número digitado é {}, o dobro é {}, o triplo {} e a raiz quadrada {}'.format(n,n1,n2,n3))
+
+#exercício 3
+n = float(input('Digite sua nota de exatas:'))
+n1 = float(input('Digite sua nota de humanas'))
+s = n + n1
+m = s / 2
+print('Em Exatas nota: {} \n Em Humanas nota: {} \n Calculo das notas: {} \n Assim ficando com a média de {}'.format(n,n1,s,m))
+
+#exercício 4
+a = float(input('Digite sua altura:'))
+c = a * 100
+m = a * 1000
+print('Sua alturo é de {} metros \n sendo {} centimetros \n {} milimetros'.format(a, c, m))
+
+

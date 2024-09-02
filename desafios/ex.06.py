326eb74fe1f8629a0131ffdd295f010b1eafce07
kaue rodrigues <iggcode@gmail.com>
2024-09-02 17:20:57 -0300

    exercicios

--- desafios/ex.06.py
+++ desafios/ex.06.py
@@ -12,3 +12,12 @@ if jogador == computador:
     print('PARABÉNS! Você conseguiu me vencer')
 else:
     print('GANHEI! Eu pensei no número {} e não no {}'.format(computador, jogador))
+    
+    
+# exercicio 2     Radar eletronico
+velocidade = float(input('Qual é a velocidade atual do carro?'))
+if velocidade > 80:
+    print('Multado! Você excedeu o limite permitido que é de 80Km/h')
+    multa = (velocidade-80) * 7
+    print('Você deve pagar uma multa de R${:.2f}'.format(multa))
+print('Tenha um bom dia! Dirija com segurança')
